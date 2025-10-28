dccf0745732884f9fb69b3b043ef9b118dca9f77
Kobayashi Ayu <a_kobayashi@casareal.co.jp>
2025-10-29 00:56:56 +0900

    rotes.jsxに編集画面のパス追加

--- src/routes.jsx
+++ src/routes.jsx
@@ -33,6 +33,10 @@ const routes = [
                 path: "new",
                 element: <EmployeeForm />,
             },
+            {
+                path: "edit/:id",
+                element: <EmployeeForm />,
+            }
         ],
     },
 ];
